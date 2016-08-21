7a2296bde80ec85244f96d329694034b0a058440
heshengtaoqw <heshengtaoqw@126.com>
2016-08-21 16:38:34 +0800

    test github

--- SeleniumTest2/src/testNG/NewTest.java
+++ SeleniumTest2/src/testNG/NewTest.java
@@ -5,6 +5,6 @@ import org.testng.annotations.Test;
 public class NewTest {
   @Test
   public void f() {
-	  	System.out.println("this is a test");
+	  	System.out.println("this is a test for github");
   }
 }
