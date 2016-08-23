36c311b93af8c5e0c1ba4e2ca3f4b41db51b0478
lic <lic1@tritonsfs.com>
2016-08-23 13:46:28 +0800

    commit the environment which is passed in local

---
+++ workspace/TestAnt-TestNG/src/testAntTestNG/BaiduAntTestNG.java
@@ -0,0 +1,51 @@
+package testAntTestNG;
+
+import org.testng.annotations.Test;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.AfterClass;
+import org.testng.asserts.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.*;
+import org.openqa.selenium.WebDriver;
+
+public class BaiduAntTestNG {
+  @Test
+  public void f() {
+	  String searchCondition = "JAVA + SELENIUM";
+		WebDriver browser = new FirefoxDriver();
+		browser.get("https://www.baidu.com");
+		
+		WebElement findIdKw = browser.findElement(By.id("kw"));
+		WebElement findIDSu = browser.findElement(By.id("su"));
+		
+		findIdKw.sendKeys(searchCondition);
+		findIDSu.click();
+		//navigation.back();
+		
+		try
+		{
+			Thread.sleep(5000);
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+		
+		
+		WebElement findRS = browser.findElement(By.className("tt"));
+		String s = (findRS.getText());
+		
+		try
+		{
+			Assert.assertEquals(s, "Ïà¹ØËÑË÷", "Assertion Failed,");
+		}
+		catch(Exception e)
+		{			
+			e.printStackTrace();
+		}
+		
+		browser.quit();
+  }
+}
